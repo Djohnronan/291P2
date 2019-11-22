cc61bd24c506c5ffe9bab319c650b5bd9037ab70
Djohnronan <dronan@ualberta.ca>
2019-11-22 15:46:33 -0700

    function gets queries

--- phase3.py
+++ phase3.py
@@ -1,8 +1,34 @@
 from bsddb3 import db
+import re
 
 def main():
-    pass
+
+    keyWords = ["subj", "body", "from",  "to",  "cc", "bcc", "date"]
+
+    command = input("> ")
+
+    commandStrip  ="".join(sommand.split())
+    commandClean = re.split(r'[^-%/\w]+', command)
+
+    queries = []
+    added = []
+
+    for i in range(len(commandClean)):
+        if commandClean[i] in keyWords:
+            prefix = commandClean[i]
+            operator = commandStrip[commandStrip.find(commandClean[i]) + len(prefix)]
+            data = commandClean[i+1]
+            queries.append(prefix+operator+data)
+            added.append(commandClean[i])
+            added.append(commandClean[i+1])
+
+    for term in commandClean:
+        if term not in added:
+            queries.append(term)
+  
+            
 
 
+    
 if __name__ == "__main__":
     main()
